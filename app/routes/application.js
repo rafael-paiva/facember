13e93c02ab240fa97028ba261662ebe616ee6d51
Rafael Paiva <rpaiva@avenuecode.com>
2015-10-20 18:35:00 -0200

    Minor refactoring; Remove debugger;

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,11 +4,7 @@ var ApplicationRoute = Ember.Route.extend({
   model: function () {
     return Ember.RSVP.hash({
         me: this.store.findRecord('user', 'me'),
-        friends: this.store.findAll('friendship'),
         friendsRequest: this.store.findAll('request')
-    }).catch((reason) => {
-      console.log('Error loading application route: ', reason);
-      this.transitionTo('login');
     });
   },
 
